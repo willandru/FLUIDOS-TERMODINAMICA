117fb279583eb6fee0c2b0d32230bd6811ab8fa2
willandru <willandru@hotmail.com>
2021-11-10 15:11:21 -0500

    Last homewokr

---
+++ 1 Ley Termodinamica/primero.m
@@ -0,0 +1,8 @@
+% Un gas ideal se comprime lentamente a una presión constante de 2,0 atm, de 10,0 L
+% a 2,0 L. Este proceso se representa en la figura como la trayectoria B a D. (En este
+% proceso, parte del calor fluye hacia fuera del gas y la temperatura disminuye.) Entonces
+% se agrega calor al gas, manteniendo el volumen constante, y se permite que la presión y
+% la temperatura (línea DA) se eleven hasta que la temperatura alcance su valor original
+% (T A = T B ). Calcule:
+% (a) El trabajo total que realiza el gas en el proceso BDA
+% (b) El flujo de calor total en el gas.
