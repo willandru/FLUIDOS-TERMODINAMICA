6082f7f9e43d1224c07c69c929e26fc33b567cae
willandru <willandru@hotmail.com>
2021-11-26 21:34:07 -0500

    Once y DOCE: solo copié enunciado

---
+++ TALLER_REPASO_FINAL/T1_11.m
@@ -0,0 +1,12 @@
+clc; clear;
+%%ONCE:
+
+% El volumen de 1.00kg de agua líquida en el rango de temperatura de 0.0 °C
+% a 50.0 °C se ajuta razonablemente a la funcion polinomica V=
+% 1.00016-(4.52e-5)T+ (5.68e-6)T², en donde el volumen s emide en metro
+% cubicos t kT se mide en grados celsius
+
+%a) Use esta informacion para calcular el coeficiente de expansion
+%volumetrica para ekl agua loiquido como funcion de la temperatrua
+
+%b)Evalue su expresion a 20,0°C y compara el valor teorico
---
+++ TALLER_REPASO_FINAL/T1_12.m
@@ -0,0 +1,13 @@
+clc; clear;
+%% DOCE;
+
+% uN BLOQUE DE COBRE DE 1.0 KG A 80.0 °c SE DEJA CAER EN UN RECIPIENTE CON
+% 2.00 L DE AGUA A 10.0 °c. cOMPARE LA MANGINTUD DEL CAMBIO DE ENERGÍA DEL
+% COBRE, con la magnitud del cambio de energía del agua. ¿Cuál valor es
+% mayor?
+
+m_cobre=1.0; %kg
+T_cobre= 80.0 ;%°C
+
+V=2.0; %L
+T_agua= 10.0; %°C
